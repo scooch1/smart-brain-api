5f6e61e61b9dde0e667a7b029d7ccad828a16068
Daniel Hangan <42680899+danielhangan@users.noreply.github.com>
2021-02-24 08:56:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex({
     connection: {
       host : '127.0.0.1',
       user : 'postgres',
-      password : 'scooch21',
+      password : 'DB-PASSWORD',
       database : 'smart-brain'
     }
   });
